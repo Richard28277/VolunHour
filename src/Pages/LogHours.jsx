2271cafb3a57dd827953f202712b3031643b61c4
Richard Xu <richardxu53@gmail.com>
2023-10-28 21:38:58 -0400

    error in log hours

--- src/Pages/LogHours.jsx
+++ src/Pages/LogHours.jsx
@@ -1,23 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'; // Assuming you're using React Router for routing
+import { useParams } from 'react-router-dom';
 import { auth } from '../firebase';
 
 const LogHours = () => {
-  const { eventName, eventHours, eventOrg } = useParams(); // Extract event name and hours from URL parameters
+  const { eventName, eventHours, eventOrg } = useParams();
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
-        // User is signed in
         setUser(authUser);
       } else {
-        // User is not signed in
         setUser(null);
       }
     });
 
-    // Clean up subscription on unmount
     return () => unsubscribe();
   }, []);
 
@@ -29,12 +26,10 @@ const LogHours = () => {
       }
 
       const user_id_token = await user.getIdToken();
-      
-      // Replace with the desired event name and hours
       const event_name = eventName;
-      const event_hours = eventHours;
       const event_org = eventOrg;
-      
+      const event_hours = parseFloat(eventHours);
+
       const response = await fetch('https://rich28277.pythonanywhere.com/api/loghours', {
         method: 'POST',
         headers: {
@@ -42,12 +37,10 @@ const LogHours = () => {
         },
         body: JSON.stringify({ user_id: user_id_token, event_org, event_name, event_hours }),
       });
-      
 
       if (response.ok) {
         console.log('Hours logged successfully.');
         window.location.href = '/dashboard';
-        // Handle success, such as redirecting to the dashboard page
       } else {
         throw new Error(`Error: ${response.status}`);
       }
